2054fbd53c24d64616b3ad9d8900bce898d6a318
racharahul <racharahul456@gmail.com>
2023-03-22 18:16:33 +0530

    Slug UI Update

--- src/pages/clubs/[clubSlug].tsx
+++ src/pages/clubs/[clubSlug].tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import type { Club } from "../../util/racha";
 import FakeApiCall from "../../util/racha";
 import Image from "next/image";
+import gstudio from "../../../assets/gstudio.png";
 import Header from "../../components/layout/header";
 
 function ClubHomePage() {
@@ -33,83 +34,84 @@ function ClubHomePage() {
   if (error || !club) return <h1>Error {error}</h1>;
   return (
     <div>
-      <div className="card mb-3 m-2">
-        <div className="card-body">
-          <h5 className="card-title">{club.name}</h5>
-          <p className="card-text">{club.description}</p>
+      <Header pageName={club.name} />
+      <br />
+      <br />
+      <br />
 
-          {/* NavTabs */}
-          <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link active"
-                id="home-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#home-tab-pane"
-                type="button"
-                role="tab"
-                aria-controls="home-tab-pane"
-                aria-selected="true"
-              >
-                Followers
-              </button>
-            </li>
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link"
-                id="profile-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#profile-tab-pane"
-                type="button"
-                role="tab"
-                aria-controls="profile-tab-pane"
-                aria-selected="false"
-              >
-                Events
-              </button>
-            </li>
-          </ul>
-          <div className="tab-content" id="myTabContent">
-            <div
-              className="tab-pane fade show active"
-              id="home-tab-pane"
-              role="tabpanel"
-              aria-labelledby="home-tab"
-              tabIndex={0}
-            >
-              ....
-            </div>
-            <div
-              className="tab-pane fade"
-              id="profile-tab-pane"
-              role="tabpanel"
-              aria-labelledby="profile-tab"
-              tabIndex={0}
-            >
-              ...
-            </div>
-            <div
-              className="tab-pane fade"
-              id="contact-tab-pane"
-              role="tabpanel"
-              aria-labelledby="contact-tab"
-              tabIndex={0}
-            >
-              ...
-            </div>
-            <div
-              className="tab-pane fade"
-              id="disabled-tab-pane"
-              role="tabpanel"
-              aria-labelledby="disabled-tab"
-              tabIndex={0}
-            >
-              ...
+      <div className="card mb-3 m-3">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <Image
+              style={{ height: "200px", width: "200px" }}
+              className="card-img-top"
+              src={gstudio}
+              alt="Card image cap"
+            ></Image>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h1 className="card-title">{club.name}</h1>
+              <h6 className="card-text">{club.description}</h6>
             </div>
           </div>
-
-          {/* NavTabs End */}
         </div>
+      </div>
+
+      <div>
+        {/* NavTabs */}
+        <ul className="nav nav-tabs" id="myTab" role="tablist">
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link active"
+              id="home-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#home-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="home-tab-pane"
+              aria-selected="true"
+            >
+              Followers
+            </button>
+          </li>
+          <li className="nav-item" role="presentation">
+            <button
+              className="nav-link"
+              id="profile-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#profile-tab-pane"
+              type="button"
+              role="tab"
+              aria-controls="profile-tab-pane"
+              aria-selected="false"
+            >
+              Events
+            </button>
+          </li>
+        </ul>
+        <div className="tab-content" id="myTabContent">
+          <div
+            className="tab-pane fade show active"
+            id="home-tab-pane"
+            role="tabpanel"
+            aria-labelledby="home-tab"
+            tabIndex={0}
+          >
+            <div className="m-2">Hello</div>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="profile-tab-pane"
+            role="tabpanel"
+            aria-labelledby="profile-tab"
+            tabIndex={0}
+          >
+            ...
+          </div>
+        </div>
+
+        {/* NavTabs End */}
       </div>
     </div>
   );
--- src/util/racha.ts
+++ src/util/racha.ts
@@ -44,7 +44,7 @@ const clubs: Club[] = [
   },
 ];
 export default async function FakeApiCall(url: string, clubSlug: string) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 100));
   const club = clubs.find(
     (club) => club.name.toLowerCase() === clubSlug.toLowerCase()
   );

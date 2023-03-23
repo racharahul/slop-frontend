81f226a96736c08a30200c49eee769a1bed880e1
jaideep-c <jaideepcvl@gmail.com>
2023-03-23 13:36:29 +0530

    changed auth provider

--- src/components/authProvider.tsx
+++ src/components/authProvider.tsx
@@ -3,20 +3,38 @@ import Cookies from "js-cookie";
 
 type ProviderValue = {
   authState: string;
+  userId: string;
   isUserAuthenticated(): boolean;
-  setAuthState(userAuthInfo: string | undefined): void;
+  setAuthState(userAuthInfo: TokenInfo | undefined): void;
 };
-
+export type TokenInfo = {
+  id: string;
+  registrationId: string;
+  fullName: string;
+  emailId: string;
+  phoneNumber: string;
+  bio: string;
+  profilePicture: string;
+  userRole: "USER" | "CLUB" | "ADMIN";
+  userSchool: string;
+  userSpecialization: string;
+  jwt: string;
+};
 export const AuthContext = React.createContext<ProviderValue>(undefined!);
 
 const AuthProvider: React.FC<any> = ({ children }) => {
   const [authState, setAuthState] = React.useState<string>(
     Cookies.get("token") ?? ""
   );
-
-  const setUserAuthInfo = (data: string) => {
-    Cookies.set("token", data, { sameSite: "lax" });
+  const [userId, setUserId] = React.useState<string>(
+    Cookies.get("userId") ?? ""
+  );
+  const setUserAuthInfo = (data: TokenInfo) => {
+    Cookies.set("token", data.jwt, { sameSite: "lax" });
+    Cookies.set("userId", data.id, { sameSite: "lax" });
+    const id = Cookies.get("userId");
     const token = Cookies.get("token");
+    setUserId(id ?? "");
     setAuthState(token ?? "");
   };
 
@@ -30,7 +48,8 @@ const AuthProvider: React.FC<any> = ({ children }) => {
 
   const appContext: ProviderValue = {
     authState,
-    setAuthState(userAuthInfo: string | undefined) {
+    userId,
+    setAuthState(userAuthInfo: TokenInfo | undefined) {
       if (userAuthInfo === undefined) {
         removeUserAuthInfo();
         return;
